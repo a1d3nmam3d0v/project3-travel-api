64f420300ca1e57bb60f9a7bce15233c18f39308
David <zemler.david@protonmail.com>
2022-10-16 21:20:12 -0500

    set up basic database structure

--- bookmark_store.py
+++ bookmark_store.py
@@ -1,1 +1,21 @@
-# test
+from peewee import *
+
+db = SqliteDatabase('bookmarks.sqlite')
+
+class BookmarkType(Model):
+    id = IntegerField()
+    name = CharField()
+
+    class Meta:
+        database = db
+
+class Bookmark(Model):
+    name = CharField()
+    url = CharField()
+    type_id = ForeignKeyField(BookmarkType, backref='bookmarks')
+    
+    class Meta:
+        database = db
+
+
+        
