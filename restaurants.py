934d271e549090ceaffd611fc4d90d26fad206c6
David <zemler.david@protonmail.com>
2022-10-26 18:26:30 -0500

    added filtering function

--- restaurants.py
+++ restaurants.py
@@ -8,7 +8,7 @@ headers = {'Authorization' : token}
 
 def get_restaurants(city, country):
     try:
-        query = {'term': 'restaurants', 'location': f'{city} {country}', 'sort_by': 'rating', 'limit': 3}
+        query = {'term': 'restaurants', 'location': f'{city} {country}', 'sort_by': 'rating', 'limit': 25}
         response = requests.get(url, params=query, headers=headers)
         response.raise_for_status()
         data = response.json()
@@ -19,12 +19,18 @@ def get_restaurants(city, country):
         print(response.text)
         return None
 
+def filter_restaurants(city, country, restaurant_data):
+    restaurant_list = []
 
+    for restaurant in restaurant_data:
+        if restaurant.location.city == city and restaurant.location.country == country:
+            restaurant_list.append(restaurant)
+            if len(restaurant_list) == 3:
+                break
 
+    return restaurant_list
 
-
-
-
-   
-
-
+data = get_restaurants('us', 'saint paul')
+print(data)
+restaurant_list = filter_restaurants('us', 'saint paul', data)
+print(restaurant_list)
