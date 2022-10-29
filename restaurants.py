4f3f176356eec93437cd24a498734c4f5903fc14
David <zemler.david@protonmail.com>
2022-10-28 21:50:17 -0500

    fixed filtering function

--- restaurants.py
+++ restaurants.py
@@ -1,3 +1,4 @@
+import re
 from urllib import response
 import requests
 import os
@@ -6,9 +7,12 @@ url = 'https://api.yelp.com/v3/businesses/search'
 token = os.environ.get('yelp_token')
 headers = {'Authorization' : token}
 
-def get_restaurants(city, country):
+def get_restaurants(city, country, result_limit):
+    city = clean_up_input(city)
+    country = clean_up_input(country)
+
     try:
-        query = {'term': 'restaurants', 'location': f'{city} {country}', 'sort_by': 'rating', 'limit': 25}
+        query = {'term': 'restaurants', 'location': f'{city}+{country}', 'sort_by': 'rating', 'limit': result_limit}
         response = requests.get(url, params=query, headers=headers)
         response.raise_for_status()
         data = response.json()
@@ -20,17 +24,17 @@ def get_restaurants(city, country):
         return None
 
 def filter_restaurants(city, country, restaurant_data):
+    city = clean_up_input(city)
+    country = clean_up_input(country)
     restaurant_list = []
 
     for restaurant in restaurant_data:
-        if restaurant.location.city == city and restaurant.location.country == country:
+        if restaurant['location']['city'].lower() == city and restaurant['location']['country'].lower() == country:
             restaurant_list.append(restaurant)
             if len(restaurant_list) == 3:
                 break
 
     return restaurant_list
 
-data = get_restaurants('us', 'saint paul')
-print(data)
-restaurant_list = filter_restaurants('us', 'saint paul', data)
-print(restaurant_list)
+def clean_up_input(text):
+    return text.strip().lower()
