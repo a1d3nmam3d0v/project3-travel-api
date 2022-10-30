ac560f09ab0810e2033221b6ad2b8be9f2889a96
David <zemler.david@protonmail.com>
2022-10-29 23:31:06 -0500

    added function to clean up data

--- restaurants.py
+++ restaurants.py
@@ -8,6 +8,12 @@ token = os.environ.get('yelp_token')
 headers = {'Authorization' : token}
 
 def get_restaurants(city, country, result_limit):
+    results = get_data(city, country, result_limit)
+    filtered_results = filter_restaurants(results)
+    restaurant_list = clean_up_restaurant_data(filtered_results)
+    return restaurant_list
+
+def get_data(city, country, result_limit):
     city = clean_up_input(city)
     country = clean_up_input(country)
 
@@ -36,5 +42,30 @@ def filter_restaurants(city, country, restaurant_data):
 
     return restaurant_list
 
+def get_categories(restaurant):
+    categories = []
+    for category in restaurant['categories']:
+        categories.append(category['title'])
+    return categories
+
+def clean_up_restaurant_data(restaurant_list):
+    cleaned_list = []
+    for restaurant in restaurant_list:
+        restaurant_data = {
+            'name': restaurant['name'],
+            'url': restaurant['url'],
+            'rating': restaurant['rating'],
+            'categories': get_categories(restaurant)
+        }
+        cleaned_list.append(restaurant_data)
+    return cleaned_list
+    
 def clean_up_input(text):
     return text.strip().lower()
+
+if __name__ == '__main__':
+    print()
+
+
+
+
