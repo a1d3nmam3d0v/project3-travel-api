37acc5dc45b9de1ddf28045126b6831bca0e0b7c
a1d3n <$>
2022-10-30 18:04:33 -0500

    Sorry for not being atomic

--- app.py
+++ app.py
@@ -1,8 +1,46 @@
 from flask import Flask, redirect, render_template, request
+import videos
+
+# import restaurants, currency
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def home_page():
+@app.route("/", methods=("GET", "POST"))
+def homepage():
     return render_template("index.html")
+
+
+@app.route("/travelguide", methods=["GET"])
+def get_travel_info():
+
+    home = request.args.get("country-from")
+    country = request.args.get("country-destination")
+    city = request.args.get("city-destination")
+
+    videos = videos.youtube_search(city, country)
+
+    return render_template(
+        "travelguide.html",
+        home="country-from",
+        country=country,
+        city=city,
+        videos=videos,
+    )
+
+
+@app.route("/bookmarks")
+def bookmark():
+    return render_template("bookmarks.html")
+
+
+#     if request.method == "get":
+
+
+#     print(city)
+#     print(country)
+#     print(home)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=5000)
--- videos.py
+++ videos.py
@@ -1,24 +1,49 @@
-# import os
-# import requests
+import os
+import requests
 
-# # key = 'AIzaSyDE-Mcze5lzysMagAiik1MgrGUjLGkqdxw'
-
-
-# key = os.environ.get("YOUTUBE_API_KEY")
-# query = {}
-# YOUTUBE_API_KEY = "AIzaSyDE-Mcze5lzysMagAiik1MgrGUjLGkqdxw"
-
-# get_url = " https://www.googleapis.com/youtube/v3/videos"
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 
-# # 
+# key = 'AIzaSyDE-Mcze5lzysMagAiik1MgrGUjLGkqdxw'
+DEVELOPER_KEY = os.environ.get("YOUTUBE_API_KEY")
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
 
 
-# request = youtube.videos().list(
-#         part="snippet,contentDetails,statistics",
-#         chart="mostPopular",
-#         regionCode="US"
-#     )
-# response = request.execute()
+def youtube_search(city, country):
+    youtube = build(
+        YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY
+    )
 
-# print(response)git 
+    destination_name = f"{city} {country} tour"
+
+    search_response = (
+        youtube.search()
+        .list(
+            q=destination_name,
+            part="id, snippet",
+            maxResults=3,
+        )
+        .execute()
+    )
+
+    videos = []
+
+    for search_result in search_response.get("items", []):
+
+        title = search_result["snippet"]["title"]
+        video_id = search_result["id"]["videoId"]
+
+        return {"title": title, "video_id": video_id}
+
+    #     videos.append(
+    #         "%s (%s)"
+    #         % (search_result["snippet"]["title"], search_result["id"]["videoId"])
+    #     )
+
+    # print("Videos:\n", "\n".join(videos), "\n")
+
+
+if __name__ == "__main__":
+    youtube_search(city, country)
